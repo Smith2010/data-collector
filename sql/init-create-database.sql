0a65cc1b3d3e0784e8c2959243eb1b8296a104ff
smithma <smith.ma@coupang.com>
2017-11-30 16:17:34 +0800

    Initial commit

---
+++ sql/init-create-database.sql
@@ -0,0 +1,15 @@
+DROP DATABASE IF EXISTS datacollector;
+CREATE DATABASE datacollector DEFAULT CHARACTER SET utf8 COLLATE utf8_bin;
+
+-- init users
+CREATE USER 'winter'@'localhost' IDENTIFIED BY 'Cndnj37!@#';
+CREATE USER 'summer'@'localhost' IDENTIFIED BY 'Ejdnj49!@#';
+
+-- DB
+USE mysql;
+
+-- data collector Database
+INSERT INTO db (HOST,Db,USER,Select_priv,Insert_priv,Update_priv,Delete_priv,Create_priv,Drop_priv,Index_priv,Alter_priv) VALUES('%','datacollector','summer','Y','Y','Y','Y','N','N','N','N');
+INSERT INTO db (HOST,Db,USER,Select_priv,Insert_priv,Update_priv,Delete_priv,Create_priv,Drop_priv,Index_priv,Alter_priv) VALUES('%','datacollector','winter','Y','Y','Y','Y','Y','Y','Y','Y');
+
+FLUSH PRIVILEGES;
