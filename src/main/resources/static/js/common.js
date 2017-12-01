b31b2a4dc873833ac5f0a6ac67573abab099c3da
smithma <smith.ma@coupang.com>
2017-12-01 15:17:21 +0800

    add images

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -7,13 +7,13 @@
         w = w > 750 ? 750 : w;
         var newW = w / 750 * 100;
         htmlEl.style.fontSize = newW + 'px';
-    }
+    };
     fitPage();
 
     var t;
     var func = function () {
         clearTimeout(t);
         t = setTimeout(fitPage, 25);
-    }
+    };
     window.addEventListener('resize', func);
 } ();
